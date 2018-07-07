76604cdaacb5d0a5137c481066166bbdf86d2bf2
myfailemtions <myfailemtions@gmail.com>
2018-07-07 11:44:57 +0800

    Добавлена возможность обновлять категорию

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ $router->group(['prefix' => 'api/v1'], function ($router) {
 	// post new category
 	$router->post('categories', 'CategoriesController@create');
     // remove category
-    $router->post('categories/{id}', 'CategoriesController@remove');
+    $router->remove('categories/{id}', 'CategoriesController@remove');
     // update categories
     $router->put('categories/{id}', 'CategoriesController@editCategory');
     // get all posts
